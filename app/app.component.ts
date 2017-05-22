01965074ad523408a535989f4fc8b497348327ba
Srinivas Mukund Chavali <mukundcs1611@gmail.com>
2017-05-22 16:55:09 -0500

    Adding code , udemy class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {TweetComponent} from './tweet.component'
 import {TweetService} from './tweet.service'
+import {SummaryPipe} from './summary.pipe';
 
 @Component({
     selector: 'my-app',
@@ -8,14 +9,24 @@ import {TweetService} from './tweet.service'
         <div *ngFor="#tweet of tweets">
             <tweet [data]="tweet"></tweet>
         </div>
+        {{post.title}}
+        <br/>
+        {{post.body|summary}}
     `,
     directives: [TweetComponent],
-    providers: [TweetService]
+    providers: [TweetService],
+    pipes:[SummaryPipe]
 })
 export class AppComponent {
+
     tweets: any[];
     
     constructor(tweetService: TweetService){
         this.tweets = tweetService.getTweets();
+    }
+    post={
+        title:"Tutorial for beginners like me",
+        body:`This is a multilined text and after 50 characters it is trimmed down 
+        you will probably dont see this line.....`
     }
 }
