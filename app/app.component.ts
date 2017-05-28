b1588d546583614035db15c7daa10935677d52ef
Srinivas Mukund Chavali <mukundcs1611@gmail.com>
2017-05-28 10:42:17 -0500

    added new files- udemy tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,28 +4,43 @@ import {TweetService} from './tweet.service'
 import {SummaryPipe} from './summary.pipe';
 import {ZippyComponent} from './zippy.component';
 import {ZipContentService} from './zipcontent.service';
+import {SubscribeFormComponent} from './subscribe-form.component';
+
 @Component({
     selector: 'my-app',
     template: `
+        <div>
+            Open All 
+        <i class="pull-right glyphicon"
+        [ngClass]="{
+            'glyphicon-chevron-down':hideU,
+            'glyphicon-chevron-up':!hideU
+            }"
+        (click)="hide()"></i>
+        </div>    
+        <div *ngIf="!hideU">
         <div *ngFor="#tweet of tweets">
             <tweet [data]="tweet"></tweet>
         </div>
-        {{post.title}}
-        <br/>
         {{post.body|summary}}
         <div *ngFor="#content of zipContents">
             <zippy [title]="content.title">
                 {{content.content}}
             </zippy>
         </div>
+        </div>
+        <div class="form-elem">
+        <subscribe></subscribe>
+        </div>
+        
         
     `,
-    directives: [TweetComponent,ZippyComponent],
+    directives: [TweetComponent,ZippyComponent,SubscribeFormComponent],
     providers: [TweetService,ZipContentService],
     pipes:[SummaryPipe]
 })
 export class AppComponent {
-
+    hideU=true;
     tweets: any[];
     zipContents: any[];
     
@@ -38,5 +53,8 @@ export class AppComponent {
         title:"Tutorial for beginners like me",
         body:`after 50 characters it is trimmed down 
         you will probably dont see this line.....`
+    }
+    hide(){
+        this.hideU=!this.hideU;
     }
 }
